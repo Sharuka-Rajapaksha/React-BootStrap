4615cf2beea9669be3128d4ce35dd278208d03d2
Sharuka Rajapaksha <rajapakshasharuka@gmail.com>
2023-11-16 21:48:33 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Main from './Components/Main';
-
+import Page2 from './Components/Page2';
 
 function App() {
   return (
     <div className="App">
 
+      <Page2/>
       <Main/>
+      
       
     </div>
   );
